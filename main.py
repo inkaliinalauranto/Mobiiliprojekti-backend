9b2f6b96972c3d652339c82a876454eb5a0a6a9c
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2024-03-30 12:37:03 +0200

    Sisällytetty akku-tagin routet app-instanssiin, mikä aiemmassa commitissa unohtui.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from router import consumption_router, production_router
+from router import consumption_router, production_router, battery_router
 
 app = FastAPI()
 app.include_router(consumption_router)
 app.include_router(production_router)
-
+app.include_router(battery_router)
