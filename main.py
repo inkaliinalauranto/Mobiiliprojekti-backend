11edc9e3f0203d179c08aaba1629084579eec311
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2024-03-30 12:39:02 +0200

    Muutettu tagien sisällyttämisjärjestystä app-instanssiin.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 from router import consumption_router, production_router, battery_router
 
 app = FastAPI()
+app.include_router(battery_router)
 app.include_router(consumption_router)
 app.include_router(production_router)
-app.include_router(battery_router)
