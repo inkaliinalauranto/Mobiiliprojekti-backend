aba18bf7b1cebbab8d1b90626939e1b268140244
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2024-03-31 15:11:06 +0300

    Päivän keskiarvolämpötilat palauttavan routen funktioon lisätty Miikan custom-funktio, jolla saadaan 0-arvo sellaiselle tunnille, jolle ei tietokannasta mahdollisesti löytyisi arvoa.

--- routers/temperature.py
+++ routers/temperature.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from sqlalchemy import text
+
+from customfunctions import generate_zero_for_missing_hours_in_day_query
 from db import DW
 
 router = APIRouter(
@@ -8,21 +10,25 @@ router = APIRouter(
 )
 
 
-# Haetaan annetun päivämäärän keskiarvolämpötilat, jotka lajitellaan
-# tuntikohtaisesti.
-@router.get("/avg/hourly_by_a_day/{date}")
-async def get_temperature_statistics_hourly_by_day(dw: DW, date: str):
+# Haetaan annetusta ajankohdasta 24 tunnin takaiset keskiarvolämpötilat,
+# jotka lajitellaan tuntikohtaisesti.
+@router.get("/avg/24_hours_before_date/{date}")
+async def get_temperature_statistics_hourly_24_before_date(dw: DW, date: str):
     """
-    Get daily temperatures (avg). String ISO 8601 format YYYY-MM-DD
+    Get hourly temperatures (avg) from 24 hours before the given date.
+    String ISO 8601 format YYYY-MM-DD.
     """
-    _query = text("SELECT DATE(TIMESTAMP(CONCAT_WS('-', d.year, d.month, d.day))) as date, avg(value) AS temperature "
+    _query = text("SELECT DATE(TIMESTAMP(CONCAT_WS('-', d.year, d.month, d.day))) as date, "
+                  "d.hour, avg(value) AS temperature "
                   "FROM temperatures_fact t "
                   "JOIN dates_dim d ON t.date_key = d.date_key "
-                  "WHERE DATE(TIMESTAMP(CONCAT_WS('-', d.year, d.month, d.day))) = :date "
+                  "WHERE DATE(TIMESTAMP(CONCAT_WS('-', d.year, d.month, d.day))) = DATE(:date) "
                   "GROUP BY d.hour "
                   "ORDER BY d.hour;")
     rows = dw.execute(_query, {"date": date})
-    data = rows.mappings().all()
+    fetched_data = rows.mappings().all()
+
+    data = generate_zero_for_missing_hours_in_day_query(fetched_data)
 
     return {"data": data}
 
