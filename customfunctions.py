bdf0b5a7b8933cf93f966026a551dbd8d7defad5
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2024-04-03 08:34:22 +0300

    Päivän keskiarvolämpötilan tunnittaishakua muokattu sanitoinnin ja 0-tietueiden haun osalta. Lisätty viimeiseen sisälämpötilatietoon liittyvä route ja funktio.

--- customfunctions.py
+++ customfunctions.py
@@ -117,33 +117,6 @@ def generate_zero_for_missing_hours_in_day_query(fetched_data):
     return data
 
 
-# Nollatietueet lämpötilaan liittyville hourly by day hauille. Syötä queryssä
-# saatu data. Datan pitää olla muotoa [{"hour": 0 "temperature": 0}]
-def generate_zero_for_missing_hours_in_day_for_temperature_query(fetched_data):
-    # Haetaan tietokannasta saadusta datasta tunnit, joissa on dataa
-    hours_fetched = [i["hour"] for i in fetched_data]
-
-    # Alustetaan new_data list ja index(käytetään returned_datan tiedon hakemisessa)
-    data = []
-    index = 0
-
-    # Silmukoidaan vuorokauden tuntien verran
-    for hour in range(24):
-
-        # Jos tunti ei löydy saadusta listasta, luodaan nolla tietue.
-        if hour not in hours_fetched:
-            hourly_data = {"hour": hour, "temperature": 0}
-
-        # Muussa tapauksessa listataan tietokannasta tullut datasta
-        else:
-            hourly_data = fetched_data[index]
-            index += 1
-
-        data.append(hourly_data)
-
-    return data
-
-
 # Generoidaan nollatietue 7 päivän jaksoon, jos tietoja ei löydy kyseiselle päivälle.
 # Vaatii datetime objectin ja SQL querystä tulleen datan
 def generate_zero_for_missing_days_in_7_day_period(fetched_data, date: datetime.date):
--- routers/temperature.py
+++ routers/temperature.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 from sqlalchemy import text
 
-from customfunctions import generate_zero_for_missing_hours_in_day_query, \
-    generate_zero_for_missing_hours_in_day_for_temperature_query
+from customfunctions import generate_zero_for_missing_hours_in_day_with_keys
 from db import DW
 
 router = APIRouter(
@@ -19,17 +18,69 @@ async def get_indoor_avg_temperature_statistics_hourly_by_day(dw: DW, date: str)
     Get hourly temperatures (avg) from a given day.
     String ISO 8601 format YYYY-MM-DD.
     """
-    _query = text("SELECT d.hour, avg(value) AS temperature "
+    _query = text("SELECT d.hour, AVG(value) AS °C "
                   "FROM temperatures_fact t "
                   "JOIN dates_dim d ON t.date_key = d.date_key "
                   "WHERE DATE(TIMESTAMP(CONCAT_WS('-', d.year, d.month, d.day))) = DATE(:date) "
-                  "AND t.sensor_key = 125 "
+                  "AND t.sensor_key = :sensor_key "
                   "GROUP BY d.hour "
                   "ORDER BY d.hour;")
-    rows = dw.execute(_query, {"date": date})
+    rows = dw.execute(_query, {"date": date, "sensor_key": 125})
     fetched_data = rows.mappings().all()
 
-    data = generate_zero_for_missing_hours_in_day_for_temperature_query(fetched_data)
+    time_key = "hour"
+    temperature_unit_key = "°C"
+
+    if len(fetched_data) > 0:
+        time_key = tuple(fetched_data[0].keys())[0]
+        temperature_unit_key = tuple(fetched_data[0].keys())[1]
+
+    data = generate_zero_for_missing_hours_in_day_with_keys(fetched_data, time_key, temperature_unit_key)
 
     return {"data": data}
+
+
+# Haetaan viimeisin lämpötilatieto:
+@router.get("/indoor/current")
+async def get_most_recent_indoor_temperature(dw: DW):
+    """
+    Get the most recent indoor temperature information.
+    String ISO 8601 format YYYY-MM-DD.
+    """
+    _query = text("SELECT CONCAT_WS(': ', s.device_name, s.sensor_name) AS sensor, t.value "
+                  "FROM sensors_dim s "
+                  "JOIN temperatures_fact t ON s.sensor_key = t.sensor_key "
+                  "WHERE t.date_key = "
+                  "(SELECT MAX(date_key) FROM temperatures_fact WHERE sensor_key = :sensor_key);")
+    rows = dw.execute(_query, {"sensor_key": 125})
+    data = rows.mappings().all()
+
+    if len(data) == 0:
+        data = [{"value": 0}]
+
+    return {"data": data}
+
+
+# # Testi
+# @router.get("/indoor/wind/nothing")
+# async def get_most_recent_wind_nothing(dw: DW):
+#     """
+#     Get the most recent winfinformation.
+#     String ISO 8601 format YYYY-MM-DD.
+#     """
+#     _query = text("SELECT p.value FROM productions_fact p "
+#                   "JOIN sensors_dim s ON p.sensor_key = s.sensor_key "
+#                   "WHERE p.date_key = (SELECT MAX(date_key) FROM productions_fact WHERE sensor_key =  286);")
+#     rows = dw.execute(_query)
+#     data = rows.mappings().all()
+#
+#     if len(data) == 0:
+#         print("DATA", data)
+#
+#     # Tästä tulee internal server error, koska jos tietueita ei ole, ei
+#     # listassa ole myöskään alkioita eli ei edes alkiota indeksillä 0
+#     if data[0]["value"] is None:
+#         print("DATA2", data)
+#
+#     return {"data": data}
 
